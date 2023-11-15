5861ef96df4dbe6c2d11b185dc57c555e4b33e2e
Charbel Sonon <charbelsnn@gmail.com>
2023-11-15 12:30:12 +0100

    add constants

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,10 @@
+async function Page() {
+    return
+    (
+        <main>
+            <h1 className="head-text">Inscription</h1>
+        </main>
+    )
+}
+
+export default Page;
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,6 @@
+function RightSidebar()
+{
+    return <h1>RSB</h1>
+}
+
+export default RightSidebar;
